8d92c0da32eeedc41d510ae187d14f8aea5697e1
luifiller <luiz.fsilva@sptech.school>
2025-08-05 21:18:15 -0300

    feat: add hmr webpack hot-reload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
